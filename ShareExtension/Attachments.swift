e808b7144640b1b78fd42c07483a02625c41988a
Leon Böttger <leon.boettger@icloud.com>
2025-08-17 21:09:40 +0200

    Fix attachment loading for macOS app

--- ShareExtension/Attachments.swift
+++ ShareExtension/Attachments.swift
@@ -16,7 +16,6 @@ func loadAttachments(with extensionContext: NSExtensionContext?, loadedItems: @e
     
     guard let extensionContext = extensionContext else { fatalError() }
     
-    
     log("Loading attachments...")
     
     var result = AttachmentDetails(urls: [], textToSend: nil, shortDescription: "", previewImage: nil)
@@ -48,58 +47,34 @@ func loadAttachments(with extensionContext: NSExtensionContext?, loadedItems: @e
         } else {
             for attachment in attachments {
                 
-                    attachment.loadItem(forTypeIdentifier: attachment.registeredTypeIdentifiers.first!, options: nil) { data, _ in
-                        
+                if attachment.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
+                    attachment.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil) { data, _ in
                         if let urlData = data as? Data,
                            let url = URL(dataRepresentation: urlData, relativeTo: nil, isAbsolute: false) {
-
-                            log("Found URL from Data: \(url)")
-                            result.urls += [url]
+                            result.urls += [(url as URL)]
+                        } else if let url = data as? NSURL {
+                            result.urls += [(url as URL)]
                         }
-                        else if let url = data as? NSURL {
+                        
+                        checkIfAttachmentsLoaded(result: result, attachments: attachments)
+                    }
+                }
+                else {
+                    attachment.loadItem(forTypeIdentifier: attachment.registeredTypeIdentifiers.first!, options: nil) { data, _ in
+                        
+                        if let url = data as? NSURL {
                             log("Found NSURL: \(url)")
                             result.urls += [(url as URL)]
+                            
+                            checkIfAttachmentsLoaded(result: result, attachments: attachments)
                         }
                         else {
                             log("Attachment is not a URL, ignoring.")
                             let cancelError = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
                             extensionContext.cancelRequest(withError: cancelError)
                         }
-                        
-                        if result.urls.count == attachments.count {
-                            if result.urls.count == 1 {
-                                
-                                if result.urls[0].isFileURL {
-                                    result.shortDescription = result.urls[0].lastPathComponent
-                                    #if os(macOS)
-                                    result.previewImage = NSWorkspace.shared.icon(forFile: result.urls[0].path)
-                                    #else
-                                    result.previewImage = Image(systemName: "document")
-                                    #endif
-                                }
-                                else if result.urls[0].scheme == "http" || result.urls[0].scheme == "https" {
-                                    result.shortDescription = result.urls[0].absoluteString
-                                    #if os(macOS)
-                                    result.previewImage = NSImage(named: NSImage.networkName)
-                                    #else
-                                    result.previewImage = Image(systemName: "network")
-                                    #endif
-                                }
-                            }
-                            else {
-                                result.shortDescription = String.localizedStringWithFormat("NFiles".localized(), result.urls.count)
-                                #if os(macOS)
-                                result.previewImage = NSImage(named: NSImage.multipleDocumentsName)
-                                #else
-                                result.previewImage = Image(systemName: "document")
-                                #endif
-                            }
-                            
-                            DispatchQueue.main.async {
-                                loadedItems(result)
-                            }
-                        }
                     }
+                }
             }
         }
     }
@@ -108,5 +83,48 @@ func loadAttachments(with extensionContext: NSExtensionContext?, loadedItems: @e
         
         let cancelError = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
         extensionContext.cancelRequest(withError: cancelError)
+    }
+    
+    
+    func checkIfAttachmentsLoaded(result: AttachmentDetails, attachments: [NSItemProvider]) {
+        
+        var result = result
+        
+        if result.urls.count == attachments.count {
+            if result.urls.count == 1 {
+                
+                if result.urls[0].isFileURL {
+                    
+                    log("Single file URL found: \(result.urls[0])")
+                    
+                    result.shortDescription = result.urls[0].lastPathComponent
+                    #if os(macOS)
+                    result.previewImage = NSWorkspace.shared.icon(forFile: result.urls[0].path)
+                    #else
+                    result.previewImage = Image(systemName: "document")
+                    #endif
+                }
+                else if result.urls[0].scheme == "http" || result.urls[0].scheme == "https" {
+                    result.shortDescription = result.urls[0].absoluteString
+                    #if os(macOS)
+                    result.previewImage = NSImage(named: NSImage.networkName)
+                    #else
+                    result.previewImage = Image(systemName: "network")
+                    #endif
+                }
+            }
+            else {
+                result.shortDescription = String.localizedStringWithFormat("NFiles".localized(), result.urls.count)
+                #if os(macOS)
+                result.previewImage = NSImage(named: NSImage.multipleDocumentsName)
+                #else
+                result.previewImage = Image(systemName: "document")
+                #endif
+            }
+            
+            DispatchQueue.main.async {
+                loadedItems(result)
+            }
+        }
     }
 }
